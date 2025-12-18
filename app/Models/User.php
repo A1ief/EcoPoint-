53402418ab4db0acc8d2e49bbd0ae91b082f447f
antonysuryajaya <antony.suryajaya@gmail.com>
2025-12-19 06:03:37 +0700

    Add files via upload

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,36 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $table = 'tb_user'; 
+    protected $primaryKey = 'id_user';
+
     protected $fillable = [
-        'name',
+        'nama',
         'email',
+        'alamat',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function sampah()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Sampah::class, 'id_user', 'id_user');
+    }
+
+    public function poin()
+    {
+        return $this->hasMany(Poin::class, 'id_user', 'id_user');
     }
 }
